91e7ff890a8aa4e4035b198a5f46f031aa653e72
m.digun <m.digun@alfa2b.ru>
2022-04-14 23:04:55 +0300

    setup project scaffolding, including pages components, base layout and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello!</h1>
-    </div>
+    <Router>
+        <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+
+            <main>
+                <Routes>
+                    <Route exact path="/" element={ <Home /> } />
+                    <Route exact path="/about" element={ <About /> } />
+                    <Route exact path="/notfound" element={ <NotFound /> } />
+                    <Route exact path="/*" element={ <NotFound /> } />
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+            <h1 className='text-6xl mb-4'>Github.Поиск</h1>
+            <p className='mb-4 text-2xl font-light'>
+                Приложение на React js для поиска GitHub-профилей и просмотра информации о них.
+            </p>
+            <p className='text-lg text-gray-400'>
+                Версия <span className='text-white'>1.0.0</span>
+            </p>
+        </>
+    );
+};
+
+export default About;
